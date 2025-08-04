81fa16e4fb2880ec62015099fed2ac5524efdad7
Marcos <mcs.corazza@gmail.com>
2025-08-04 05:34:04 -0300

    Finalizado Restaurant

--- src/main/java/br/inatel/pos/dm11/vfu/api/restaurant/controller/RestaurantController.java
+++ src/main/java/br/inatel/pos/dm11/vfu/api/restaurant/controller/RestaurantController.java
@@ -1,6 +1,7 @@
 package br.inatel.pos.dm11.vfu.api.restaurant.controller;
 
 import br.inatel.pos.dm11.vfu.api.core.ApiException;
+import br.inatel.pos.dm11.vfu.api.core.AppError;
 import br.inatel.pos.dm11.vfu.api.restaurant.RestaurantRequest;
 import br.inatel.pos.dm11.vfu.api.restaurant.RestaurantResponse;
 import br.inatel.pos.dm11.vfu.api.restaurant.service.RestaurantService;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -54,7 +56,7 @@ public class RestaurantController {
 
         log.debug("Received request to create a new restaurant...");
 
-        //validateRestaurantRequest(request, bindingResult);
+        validateRequest(request, bindingResult);
         var response = service.createRestaurant(request);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -68,7 +70,7 @@ public class RestaurantController {
             BindingResult bindingResult) throws ApiException {
         log.debug("Received request to update a restaurant");
 
-        //validateUserRequest(request, bindingResult);
+        validateRequest(request, bindingResult);
 
         var response = service.updateRestaurant(request, id);
 
@@ -83,5 +85,13 @@ public class RestaurantController {
         service.removeRestaurant(id);
         return ResponseEntity
                 .status(HttpStatus.NO_CONTENT).build();
+    }
+
+    private void validateRequest(RestaurantRequest request, BindingResult bindingResult) throws ApiException {
+        ValidationUtils.invokeValidator(validator, request, bindingResult);
+        if(bindingResult.hasErrors()) {
+            var errors = bindingResult.getFieldErrors().stream().map(fe -> new AppError(fe.getCode(), fe.getDefaultMessage())).toList();
+            throw new ApiException(HttpStatus.BAD_REQUEST, errors);
+        }
     }
 }
--- src/main/java/br/inatel/pos/dm11/vfu/api/restaurant/controller/RestaurantRequestValidator.java
+++ src/main/java/br/inatel/pos/dm11/vfu/api/restaurant/controller/RestaurantRequestValidator.java
@@ -2,6 +2,7 @@ package br.inatel.pos.dm11.vfu.api.restaurant.controller;
 
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 @Component
@@ -13,6 +14,9 @@ public class RestaurantRequestValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "name.empty", "Name is required!");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address", "address.empty", "Address is required!");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userId", "userId.empty", "User ID is required!");
+        ValidationUtils.rejectIfEmpty(errors, "categories", "categories.empty", "Categories is required!");
     }
 }
